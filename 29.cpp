436bcdf89abf8b8a0b7b444376cb5032592f8de2
Charu-2718 <charu.15421@gmail.com>
2023-02-26 01:31:09 +0530

    //Print //1 //12 //123 //1234 //1234..

---
+++ 29.cpp
@@ -0,0 +1,21 @@
+//Print
+//1
+//12
+//123
+//1234
+//1234..
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int i,j,rows;
+    cout << " Enter the number of rows: ";
+    cin >> rows;
+   for(i=1;i<=rows;i++)
+   {
+	for(j=1;j<=i;j++)
+	   cout<<j;
+	cout<<endl;
+   }	
+}
